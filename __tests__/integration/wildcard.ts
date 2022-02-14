f2bc28d3eeaa01a0c9055c97369a554ee130ab8c
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-02-14 17:02:38 -0500

    test(integration/wildcard): remove top level describe

--- __tests__/integration/wildcard.ts
+++ __tests__/integration/wildcard.ts
@@ -9,42 +9,103 @@ afterEach(() => {
     awaiter.reset()
 })
 
-describe("test wildcard matching", () => {
-    describe("top level wildcards", () => {
-        const mockDiscord = new MockDiscord()
-        const {client} = mockDiscord
+describe("test top level wildcards", () => {
+    const mockDiscord = new MockDiscord()
+    const {client} = mockDiscord
 
-        client.initExpress()
+    client.initExpress()
 
-        client.use(...middleware.recommended())
-        client.use(middleware.messageCommandParser({prefix: "!"}))
+    client.use(...middleware.recommended())
+    client.use(middleware.messageCommandParser({prefix: "!"}))
 
-        client.command("*", () => {
-            awaiter.call()
+    client.command("*", () => {
+        awaiter.call()
+    })
+
+    test.each(["random-command", "another-command", "e"])(
+        "should match any command",
+        async (name) => {
+            const interaction = mockDiscord.mockCommandInteraction(undefined, {
+                name,
+            })
+
+            const wait = awaiter.wait(1000)
+
+            client.emit("interactionCreate", interaction)
+
+            await wait
+        },
+    )
+
+    test.each([
+        ["random-subcommand", "random-command"],
+        ["another-subcommand", "another-command"],
+        ["a", "e"],
+    ])("should match any subcommand", async (subcommand, command) => {
+        const interaction = mockDiscord.mockCommandInteraction(undefined, {
+            name: subcommand,
+            options: [
+                {
+                    type: ApplicationCommandOptionType.Subcommand,
+                    name: command,
+                },
+            ],
         })
 
-        test.each(["random-command", "another-command", "e"])(
-            "should match any command",
-            async (name) => {
-                const interaction = mockDiscord.mockCommandInteraction(undefined, {
-                    name,
-                })
+        const wait = awaiter.wait(1000)
 
-                const wait = awaiter.wait(1000)
+        client.emit("interactionCreate", interaction)
 
-                client.emit("interactionCreate", interaction)
+        await wait
+    })
 
-                await wait
-            },
-        )
+    test.each([
+        ["random-subcommand-group", "random-subcommand", "random-command"],
+        ["another-subcommand-group", "another-subcommand", "another-command"],
+        ["O", "a", "e"],
+    ])("should match any subcommand group", async (subcommandGroup, subcommand, command) => {
+        const interaction = mockDiscord.mockCommandInteraction(undefined, {
+            name: subcommandGroup,
+            options: [
+                {
+                    type: ApplicationCommandOptionType.SubcommandGroup,
+                    name: subcommand,
+                    options: [
+                        {
+                            type: ApplicationCommandOptionType.Subcommand,
+                            name: command,
+                        },
+                    ],
+                },
+            ],
+        })
 
-        test.each([
-            ["random-subcommand", "random-command"],
-            ["another-subcommand", "another-command"],
-            ["a", "e"],
-        ])("should match any subcommand", async (subcommand, command) => {
+        const wait = awaiter.wait(1000)
+
+        client.emit("interactionCreate", interaction)
+
+        await wait
+    })
+})
+
+describe("test subcommand level wildcards", () => {
+    const mockDiscord = new MockDiscord()
+    const {client} = mockDiscord
+
+    client.initExpress()
+
+    client.use(...middleware.recommended())
+    client.use(middleware.messageCommandParser({prefix: "!"}))
+
+    client.command("mySubcommand/*", () => {
+        awaiter.call()
+    })
+
+    test.each(["random-command", "another-command", "e"])(
+        "should match any subcommand",
+        async (command) => {
             const interaction = mockDiscord.mockCommandInteraction(undefined, {
-                name: subcommand,
+                name: "my-subcommand",
                 options: [
                     {
                         type: ApplicationCommandOptionType.Subcommand,
@@ -58,82 +119,60 @@ describe("test wildcard matching", () => {
             client.emit("interactionCreate", interaction)
 
             await wait
-        })
+        },
+    )
 
-        test.each([
-            ["random-subcommand-group", "random-subcommand", "random-command"],
-            ["another-subcommand-group", "another-subcommand", "another-command"],
-            ["O", "a", "e"],
-        ])("should match any subcommand group", async (subcommandGroup, subcommand, command) => {
-            const interaction = mockDiscord.mockCommandInteraction(undefined, {
-                name: subcommandGroup,
-                options: [
-                    {
-                        type: ApplicationCommandOptionType.SubcommandGroup,
-                        name: subcommand,
-                        options: [
-                            {
-                                type: ApplicationCommandOptionType.Subcommand,
-                                name: command,
-                            },
-                        ],
-                    },
-                ],
-            })
-
-            const wait = awaiter.wait(1000)
-
-            client.emit("interactionCreate", interaction)
-
-            await wait
-        })
-    })
-
-    describe("subcommand level wildcards", () => {
-        const mockDiscord = new MockDiscord()
-        const {client} = mockDiscord
-
-        client.initExpress()
-
-        client.use(...middleware.recommended())
-        client.use(middleware.messageCommandParser({prefix: "!"}))
-
-        client.command("mySubcommand/*", () => {
-            awaiter.call()
-        })
-
-        test.each(["random-command", "another-command", "e"])(
-            "should match any subcommand",
-            async (command) => {
-                const interaction = mockDiscord.mockCommandInteraction(undefined, {
-                    name: "my-subcommand",
+    test.each([
+        ["random-subcommand", "random-command"],
+        ["another-subcommand", "another-command"],
+        ["a", "e"],
+    ])("should match any subcommand group", async (subcommand, command) => {
+        const interaction = mockDiscord.mockCommandInteraction(undefined, {
+            name: "my-subcommand",
+            options: [
+                {
+                    type: ApplicationCommandOptionType.SubcommandGroup,
+                    name: subcommand,
                     options: [
                         {
                             type: ApplicationCommandOptionType.Subcommand,
                             name: command,
                         },
                     ],
-                })
+                },
+            ],
+        })
 
-                const wait = awaiter.wait(1000)
+        const wait = awaiter.wait(1000)
 
-                client.emit("interactionCreate", interaction)
+        client.emit("interactionCreate", interaction)
 
-                await wait
-            },
-        )
+        await wait
+    })
+})
 
-        test.each([
-            ["random-subcommand", "random-command"],
-            ["another-subcommand", "another-command"],
-            ["a", "e"],
-        ])("should match any subcommand group", async (subcommand, command) => {
+describe("test subcommand group level wildcards", () => {
+    const mockDiscord = new MockDiscord()
+    const {client} = mockDiscord
+
+    client.initExpress()
+
+    client.use(...middleware.recommended())
+    client.use(middleware.messageCommandParser({prefix: "!"}))
+
+    client.command("mySubcommandGroup/mySubcommand/*", () => {
+        awaiter.call()
+    })
+
+    test.each(["random-command", "another-command", "e"])(
+        "should match any subcommand group",
+        async (command) => {
             const interaction = mockDiscord.mockCommandInteraction(undefined, {
-                name: "my-subcommand",
+                name: "my-subcommand-group",
                 options: [
                     {
                         type: ApplicationCommandOptionType.SubcommandGroup,
-                        name: subcommand,
+                        name: "my-subcommand",
                         options: [
                             {
                                 type: ApplicationCommandOptionType.Subcommand,
@@ -149,47 +188,6 @@ describe("test wildcard matching", () => {
             client.emit("interactionCreate", interaction)
 
             await wait
-        })
-    })
-
-    describe("subcommand group level wildcards", () => {
-        const mockDiscord = new MockDiscord()
-        const {client} = mockDiscord
-
-        client.initExpress()
-
-        client.use(...middleware.recommended())
-        client.use(middleware.messageCommandParser({prefix: "!"}))
-
-        client.command("mySubcommandGroup/mySubcommand/*", () => {
-            awaiter.call()
-        })
-
-        test.each(["random-command", "another-command", "e"])(
-            "should match any subcommand group",
-            async (command) => {
-                const interaction = mockDiscord.mockCommandInteraction(undefined, {
-                    name: "my-subcommand-group",
-                    options: [
-                        {
-                            type: ApplicationCommandOptionType.SubcommandGroup,
-                            name: "my-subcommand",
-                            options: [
-                                {
-                                    type: ApplicationCommandOptionType.Subcommand,
-                                    name: command,
-                                },
-                            ],
-                        },
-                    ],
-                })
-
-                const wait = awaiter.wait(1000)
-
-                client.emit("interactionCreate", interaction)
-
-                await wait
-            },
-        )
-    })
+        },
+    )
 })
